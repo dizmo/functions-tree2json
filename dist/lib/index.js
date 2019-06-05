444feaa84a06c649ac79c3e7634a2d0a4fc3b674
Hasan Karahan <hasan.karahan@blackhan.com>
2019-06-05 14:18:07 +0300

    yo @dizmo/module --upgrade

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 /**
  * For a given path traverses a tree-like structure in-order, and returns an
  * array containing the sub-tree of the corresponding node.
@@ -16,25 +18,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
  *
  * @returns an array representation of sub-tree
  */
-exports.tree2array = function t2a(path, value, nodes) {
-    var separator = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "/";
 
-    if (path !== null && path.startsWith(separator)) {
-        path = path.slice(1);
-    }
-    if (path !== null && path.endsWith(separator)) {
-        path = path.slice(0, -1);
-    }
-    var my_nodes = nodes(path);
-    if (my_nodes && my_nodes.length > 0) {
-        var my_array = [value(path)];
-        my_nodes.forEach(function (node, i) {
-            my_array[i + 1] = [node, t2a("" + (path || "") + separator + node, value, nodes, separator = separator)];
-        });
-        return my_array;
-    } else {
-        return [value(path)];
-    }
+exports.tree2array = function t2a(path, value, nodes) {
+  var separator = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "/";
+
+  if (path !== null && path.startsWith(separator)) {
+    path = path.slice(1);
+  }
+
+  if (path !== null && path.endsWith(separator)) {
+    path = path.slice(0, -1);
+  }
+
+  var my_nodes = nodes(path);
+
+  if (my_nodes && my_nodes.length > 0) {
+    var my_array = [value(path)];
+    my_nodes.forEach(function (node, i) {
+      my_array[i + 1] = [node, t2a("".concat(path || "").concat(separator).concat(node), value, nodes, separator = separator)];
+    });
+    return my_array;
+  } else {
+    return [value(path)];
+  }
 };
 /**
  * For a given path traverses a tree-like structure in-order, and returns an
@@ -51,30 +57,38 @@ exports.tree2array = function t2a(path, value, nodes) {
  *
  * @returns an array representation of sub-tree
  */
-exports.tree2object = function t2o(path, value, nodes) {
-    var separator = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "/";
-    var value_key = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "_";
 
-    if (path !== null && path.startsWith(separator)) {
-        path = path.slice(1);
+
+exports.tree2object = function t2o(path, value, nodes) {
+  var separator = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "/";
+  var value_key = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "_";
+
+  if (path !== null && path.startsWith(separator)) {
+    path = path.slice(1);
+  }
+
+  if (path !== null && path.endsWith(separator)) {
+    path = path.slice(0, -1);
+  }
+
+  var my_value = value(path);
+  var my_nodes = nodes(path);
+
+  if (my_nodes && my_nodes.length > 0) {
+    var my_object = {};
+
+    if (my_value !== undefined) {
+      my_object[value_key] = my_value;
     }
-    if (path !== null && path.endsWith(separator)) {
-        path = path.slice(0, -1);
-    }
-    var my_value = value(path);
-    var my_nodes = nodes(path);
-    if (my_nodes && my_nodes.length > 0) {
-        var my_object = {};
-        if (my_value !== undefined) {
-            my_object[value_key] = my_value;
-        }
-        my_nodes.forEach(function (node) {
-            my_object[node] = t2o("" + (path || "") + separator + node, value, nodes, separator = separator, value_key = value_key);
-        });
-        return my_object;
-    } else {
-        return my_value;
-    }
+
+    my_nodes.forEach(function (node) {
+      my_object[node] = t2o("".concat(path || "").concat(separator).concat(node), value, nodes, separator = separator, value_key = value_key);
+    });
+    return my_object;
+  } else {
+    return my_value;
+  }
 };
-exports.default = exports.tree2array;
+
+exports["default"] = exports.tree2array;
 //# sourceMappingURL=index.js.map
--- dist/test/mock.js
+++ dist/test/mock.js
@@ -1,71 +1,94 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-Object.defineProperty(exports, "__esModule", { value: true });
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-var TreeMock = function () {
-    function TreeMock() {
-        _classCallCheck(this, TreeMock);
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var TreeMock =
+/*#__PURE__*/
+function () {
+  function TreeMock() {
+    _classCallCheck(this, TreeMock);
+  }
+
+  _createClass(TreeMock, null, [{
+    key: "value",
+    value: function value(path) {
+      switch (path) {
+        case null:
+          return undefined;
+
+        case "a":
+          return "α";
+
+        case "b":
+          return "β";
+
+        case "b/i":
+          return 0;
+
+        case "b/j":
+          return 1;
+
+        case "b/k":
+          return 2;
+
+        case "c":
+          return "γ";
+
+        case "c/x":
+          return "ξ";
+
+        case "c/x/y":
+          return "υ";
+
+        case "c/x/y/z":
+          return "ζ";
+
+        default:
+          return undefined;
+      }
     }
+  }, {
+    key: "nodes",
+    value: function nodes(path) {
+      switch (path) {
+        case null:
+          return ["a", "b", "c"];
 
-    _createClass(TreeMock, null, [{
-        key: "value",
-        value: function value(path) {
-            switch (path) {
-                case null:
-                    return undefined;
-                case "a":
-                    return "α";
-                case "b":
-                    return "β";
-                case "b/i":
-                    return 0;
-                case "b/j":
-                    return 1;
-                case "b/k":
-                    return 2;
-                case "c":
-                    return "γ";
-                case "c/x":
-                    return "ξ";
-                case "c/x/y":
-                    return "υ";
-                case "c/x/y/z":
-                    return "ζ";
-                default:
-                    return undefined;
-            }
-        }
-    }, {
-        key: "nodes",
-        value: function nodes(path) {
-            switch (path) {
-                case null:
-                    return ["a", "b", "c"];
-                case "a":
-                    return [];
-                case "b":
-                    return ["i", "j", "k"];
-                case "c":
-                    return ["x"];
-                case "c/x":
-                    return ["y"];
-                case "c/x/y":
-                    return ["z"];
-                case "c/x/y/z":
-                    return null;
-                default:
-                    return undefined;
-            }
-        }
-    }]);
+        case "a":
+          return [];
 
-    return TreeMock;
+        case "b":
+          return ["i", "j", "k"];
+
+        case "c":
+          return ["x"];
+
+        case "c/x":
+          return ["y"];
+
+        case "c/x/y":
+          return ["z"];
+
+        case "c/x/y/z":
+          return null;
+
+        default:
+          return undefined;
+      }
+    }
+  }]);
+
+  return TreeMock;
 }();
 
 exports.TreeMock = TreeMock;
-exports.default = TreeMock;
+exports["default"] = TreeMock;
 //# sourceMappingURL=mock.js.map
